2036ca1b7b80a4866b78c6a7ffe3b3f0d4c25c9a
Artem <artem@MacBook-Air-Artem.local>
2019-08-02 11:57:38 +0300

    Дополнительно домашнее задание с самозакрывающимся окном

---
+++ src-8/js/main.js
@@ -0,0 +1,17 @@
+var button = document.querySelector('#button');
+var modal = document.querySelector('#modal'); 
+var close = document.querySelector('#close')
+
+button.addEventListener('click', function() {
+  modal.classList.add('modal_active');
+});
+
+close.addEventListener('click', function(){
+  modal.classList.remove('modal_active'); 
+});
+
+function closePanel(){
+  modal.classList.remove('modal_active');
+}
+
+setTimeout (closePanel, 5000)
